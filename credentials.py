0c6f643c54190fe6e2cb436b387ef645be695106
RoshanADK <roshannadhikari039@gmail.com>
2017-09-23 22:43:23 +0530

    Setup Twitter app to get all these variables

---
+++ credentials.py
@@ -0,0 +1,14 @@
+
+
+# You need a twitter app set up to redirect the tweets. If you know how to do so , its fine. Or else visit 
+# http://docs.inboundnow.com/guide/create-twitter-application/
+# App access for Twitter Application
+
+# Consumer : 
+CONSUMER_KEY = 'Your Consumer KEY paste here'
+CONSUMER_SECRET = 'you know what to paste here'
+
+# Access : 
+
+ACCESS_TOKEN = 'Paste here access token'
+ACCESS_SECRET = 'paste secret'
